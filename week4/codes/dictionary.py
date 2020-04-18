39390a867d0931b934c3695397be1942beb65383
Babatunde13 <koikibabatunde14@gmail.com>
2020-04-18 20:06:03 +0100

    Update dictionary.py

--- week4/codes/dictionary.py
+++ week4/codes/dictionary.py
@@ -35,3 +35,44 @@ Dict = dict([(1, 'Geeks'), (2, 'For')])
 print("\nDictionary with each item as a pair: ") 
 print(Dict) 
 
+
+
+# Create and print a dictionary
+
+thisdict = {"brand": "Ford","model": "Mustang","year": 1964}
+print(thisdict)
+
+#Get the value of the "model" key
+x = thisdict["model"]
+
+'''
+There is also a method called get() that will give you the same result
+Get the value of the "model" key
+'''
+
+x = thisdict.get("model")
+
+#Change the "year" to 2018
+
+thisdict = {"brand": "Ford","model": "Mustang","year": 1964}
+thisdict["year"] = 2018
+print(thisdict)
+
+'''
+You can also use the values() function to return values of a dictionary
+The code will print out all the values in the key value pair in the dictionary
+'''
+for x in thisdict.values():
+  print(x)
+
+#Loop through both keys and values, by using the items() function
+for x, y in thisdict.items():
+  print(x, y)
+
+#Print the number of items in the dictionary
+print(len(thisdict))
+
+#Adding an item to the dictionary is done by using a new index key and assigning a value to it
+thisdict = {"brand": "Ford","model": "Mustang","year": 1964}
+thisdict["color"] = "red"
+print(thisdict)
