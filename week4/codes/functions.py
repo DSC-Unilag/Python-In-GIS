6b3a500463e2b093da453c1656382a87fa635e95
Babatunde13 <koikibabatunde14@gmail.com>
2020-04-18 22:07:21 +0100

    Updated functions.py

--- week4/codes/functions.py
+++ week4/codes/functions.py
@@ -23,3 +23,13 @@ def my_function(fname, lname):
   print(fname + " " + lname)
 
 my_function("Emil") # TypeError: my_function() missing 1 required positional argument: 'lname'
+
+# Default Parameter Value
+# If we call the function without argument, it uses the default value
+def my_function(country = "Norway"):
+  print("I am from " + country)
+
+my_function("Sweden") # I am from Sweden
+my_function # I am from Norway
+my_function() # I am from Norway
+my_function("Brazil")# I am from Brazil
