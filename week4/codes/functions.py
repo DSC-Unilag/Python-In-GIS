0c432737463b306f632238f66513825fc8c5801f
Babatunde13 <koikibabatunde14@gmail.com>
2020-04-18 22:05:08 +0100

    Updated functions.py

--- week4/codes/functions.py
+++ week4/codes/functions.py
@@ -1,5 +1,25 @@
  # We call a function  by writing the function name and adding parenthesis and putting the parameter, here no parameter
 def my_function():
-  print("Hello from a function")
+    print("Hello from a function")
 my_function() # Hello from a function
 
+# Adding Parameters to our Function
+def my_function(fname):
+    print(fname + " Refsnes")
+
+my_function("Emil") # Emil Refsnes
+my_function("Tobias") # Tobias Refsnes
+my_function("Linus") # Linus Refsnes
+
+# This function expects 2 arguments, and gets 2 arguments:
+def my_function(fname, lname):
+    print(fname + " " + lname)
+
+my_function("Emil", "Refsnes") # Emil Refsnes
+
+# If you try to call the function with 1 or 3 arguments, you will get an error:
+
+def my_function(fname, lname):
+  print(fname + " " + lname)
+
+my_function("Emil") # TypeError: my_function() missing 1 required positional argument: 'lname'
