90e355f437280676d169505e1a85dc32cf7aed13
Babatunde13 <koikibabatunde14@gmail.com>
2020-04-18 22:17:43 +0100

    Updated functions.py

--- week4/codes/functions.py
+++ week4/codes/functions.py
@@ -1,4 +1,4 @@
- # We call a function  by writing the function name and adding parenthesis and putting the parameter, here no parameter
+# We call a function  by writing the function name and adding parenthesis and putting the parameter, here no parameter
 def my_function():
     print("Hello from a function")
 my_function() # Hello from a function
@@ -22,7 +22,7 @@ my_function("Emil", "Refsnes") # Emil Refsnes
 def my_function(fname, lname):
   print(fname + " " + lname)
 
-my_function("Emil") # TypeError: my_function() missing 1 required positional argument: 'lname'
+# my_function("Emil") # TypeError: my_function() missing 1 required positional argument: 'lname'
 
 # Default Parameter Value
 # If we call the function without argument, it uses the default value
@@ -33,3 +33,37 @@ my_function("Sweden") # I am from Sweden
 my_function # I am from Norway
 my_function() # I am from Norway
 my_function("Brazil")# I am from Brazil
+
+# Return Values
+# To let a function return a value, use the return statement
+def my_function(x):
+  return 5 * x
+
+print(my_function(3)) # 15
+print(my_function(5)) # 25
+print(my_function(9)) # 45
+
+# Pass Statement
+# function definitions cannot be empty, but if you for some reason have a function definition 
+# with no content, put in the pass statement to avoid getting an error
+
+def myfunction():
+  pass
+
+def tri_recursion(k):
+  if(k > 0):
+    result = k + tri_recursion(k - 1)
+    print(result)
+  else:
+    result = 0
+  return result
+
+print("\n\nRecursion Example Results")
+tri_recursion(6) 
+# Output ---
+# 1
+# 3
+# 6
+# 10
+# 15
+# 21
