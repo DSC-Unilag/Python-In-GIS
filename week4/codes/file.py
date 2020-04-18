515c32aec75ce6a029ad8db6cae4ecaabd3c1579
Babatunde13 <koikibabatunde14@gmail.com>
2020-04-19 00:03:29 +0100

    new file:   __pycache__/mymodule.cpython-36.pyc modified:   file.py modified:   mymodule.py

--- week4/codes/file.py
+++ week4/codes/file.py
@@ -1,3 +1,25 @@
 import mymodule
+import mymodule as mx
+from mymodule import person1
+
+# There are several built-in modules in Python, which you can import whenever you like.
+# Import and use the platform module
+import platform
 
 mymodule.greeting("Jonathan") # Hello Jonathan
+
+a = mymodule.person1["age"]
+print(a) # 36
+
+a = mx.person1["age"] # Using aliasing
+print(a)
+
+
+x = platform.system()
+print(x) # Windows
+
+# There is a built-in function to list all the function names (or variable names) in a module. 
+x = dir(platform)
+print(x)
+
+print(person1['age']) # 36
--- week4/codes/mymodule.py
+++ week4/codes/mymodule.py
@@ -1,2 +1,9 @@
 def greeting(name):
   print("Hello, " + name)
+
+
+person1 = {
+  "name": "Babatunde",
+  "age": 36,
+  "country": "Nigeria"
+}
