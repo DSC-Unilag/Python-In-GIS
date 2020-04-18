4ea947ae2087e5aa24f19565faeb779adfbdd0b6
Sodiq Akinjobi <sodiq.akinjobi@gmail.com>
2020-04-18 17:19:47 +0100

    Create firstfile.py

---
+++ firstfile.py
@@ -0,0 +1,26 @@
+# print('My name is Sodiq')
+# This is a single line comment
+'''
+This is a multi line comment.
+I am testing multine comment
+'''
+a = 6
+b = 4
+c = a * b
+d = a / b
+e = a + b
+f = a - b
+g = b**a
+print(c)
+print(d)
+print(e)
+print(f)
+print(g)
+
+# As an assignment, read on FLOOR division, modulus and practice it
+
+#  int, float, bool, string
+
+# bool can either be true or false
+
+
