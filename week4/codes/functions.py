9fec2f4e0493526f85a540537a24e1b20908b1a9
Babatunde13 <koikibabatunde14@gmail.com>
2020-04-18 21:57:41 +0100

    Updated functions.py

---
+++ week4/codes/functions.py
@@ -0,0 +1,5 @@
+ # We call a function  by writing the function name and adding parenthesis and putting the parameter, here no parameter
+def my_function():
+  print("Hello from a function")
+my_function() # Hello from a function
+
