55e5e7772d19b1197897ec5f97d0470055e943fb
Babatunde13 <koikibabatunde14@gmail.com>
2020-04-18 19:22:46 +0100

    Dictionary file

---
+++ week4/codes/dictionary.py
@@ -0,0 +1,37 @@
+# Creating a Dictionary 
+# with Integer Keys 
+Dict = {1: 'Geeks', 2: 'For', 3: 'Geeks'} 
+print("\nDictionary with the use of Integer Keys: ") 
+print(Dict) 
+
+# Creating a Dictionary 
+# with Mixed keys 
+Dict = {'Name': 'Geeks', 1: [1, 2, 3, 4]} 
+print("\nDictionary with the use of Mixed Keys: ") 
+print(Dict) 
+
+
+## Output --- 
+## Dictionary with the use of Integer Keys: 
+## {1: 'Geeks', 2: 'For', 3: 'Geeks'}
+
+## Dictionary with the use of Mixed Keys: 
+## {1: [1, 2, 3, 4], 'Name': 'Geeks'}
+
+# Creating an empty Dictionary 
+Dict = {} 
+print("Empty Dictionary: ") 
+print(Dict) 
+
+# Creating a Dictionary 
+# with dict() method 
+Dict = dict({1: 'Geeks', 2: 'For', 3:'Geeks'}) 
+print("\nDictionary with the use of dict(): ") 
+print(Dict) 
+
+# Creating a Dictionary 
+# with each item as a Pair 
+Dict = dict([(1, 'Geeks'), (2, 'For')]) 
+print("\nDictionary with each item as a pair: ") 
+print(Dict) 
+
