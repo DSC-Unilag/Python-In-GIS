a4b6715e2c52c5be0c92e8713f4da3bb93d6d00f
Babatunde13 <koikibabatunde14@gmail.com>
2020-04-21 23:24:10 +0100

    Updated  assert_exceptions.py

--- week4/codes/assert_exceptions.py
+++ week4/codes/assert_exceptions.py
@@ -1,6 +1,13 @@
 def KelvinToFahrenheit(Temperature):
    assert (Temperature >= 0),"Colder than absolute zero!"
    return ((Temperature-273)*1.8)+32
-print(KelvinToFahrenheit(273))
-print(int(KelvinToFahrenheit(505.78)))
+
+print(KelvinToFahrenheit(273)) # 32.0
+print(int(KelvinToFahrenheit(505.78))) # 451
 print(KelvinToFahrenheit(-5))
+# Traceback (most recent call last):
+#   File "c:/Users/user/Desktop/SEG With Python/Python-In-GIS/week4/codes/assert_exceptions.py", line 6, in <module>
+#     print(KelvinToFahrenheit(-5))
+#   File "c:/Users/user/Desktop/SEG With Python/Python-In-GIS/week4/codes/assert_exceptions.py", line 2, in KelvinToFahrenheit
+#     assert (Temperature >= 0),"Colder than absolute zero!"
+# AssertionError: Colder than absolute zero!
